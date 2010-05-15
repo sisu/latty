93e5f294af370052e16394a3275e3cc4d5c00a1a
unknown <mikalait@.CSWin.cs.Helsinki.FI>
2010-05-15 21:43:21 +0300

    Something working (probes gather minerals), bye

--- Bot.cpp
+++ Bot.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace BWAPI;
 using namespace BWTA;
+using namespace UnitTypes;
 
 #define sz(x) ((int)x.size())
 
@@ -15,11 +16,12 @@ typedef UVec::iterator UVI;
 
 UVec probes;
 UVec units;
+UVec minerals;
 
 vector<BaseLocation*> areas;
 vector<vector<Unit*> > aunits;
 
-void addPyon(int x)
+void addPylon(int x)
 {
 	BaseLocation* b = areas[x];
 	vector<Unit*>& v = aunits[x];
@@ -36,8 +38,8 @@ void Bot::onStart()
 
 void updateUnitList() {
 	units.clear();
-	USet uns = Broodwar->self()->getUnits();
-	for(UVI it = units.begin(); it != units.end(); ++it) {
+	const USet& uns = Broodwar->self()->getUnits();
+	for(USCI it = uns.begin(); it != uns.end(); ++it) {
 		Unit* u = *it;
 		units.push_back(u);
 	}
@@ -47,12 +49,41 @@ void updateProbeList() {
 	probes.clear();
 	for(UVI it = units.begin(); it != units.end(); ++it) {
 		Unit* u = *it;
+		probes.push_back(u);
 	}
+}
+
+void updateMineralList() {
+	const USet& gsm = Broodwar->getStaticMinerals();
+	minerals.clear();
+	for(USCI it = gsm.begin(); it != gsm.end(); ++it) {
+		Unit* u = *it;
+		if(u->getType() == Resource_Mineral_Field) {
+			minerals.push_back(u);
+		}
+	}
+}
+
+Unit* nearestMineralSource(Unit* u) {
+	double smallestDistance = 1e50;
+	Unit* ret = NULL;
+	for(int i = 0; i < sz(minerals); ++i) {
+		double dist = u->getDistance(minerals[i]);
+		if(dist < smallestDistance) {
+			smallestDistance = dist;
+			ret = minerals[i];
+		}
+	}
+	return ret;
 }
 
 void taskifyProbes() {
 	for(int i = 0; i < sz(probes); ++i) {
-		
+		if(!probes[i]->isIdle()) continue;
+		Unit* u = nearestMineralSource(probes[i]);
+		if(u) {
+			probes[i]->rightClick(u);
+		}
 	}
 }
 
@@ -60,5 +91,7 @@ void Bot::onFrame()
 {
 	updateUnitList();
 	updateProbeList();
+	updateMineralList();
+
 	taskifyProbes();
 }
